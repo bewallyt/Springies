430cc86430126a0ea6ab070e19b94c205691cbb0
Benson Tran <BewallyT@wl-10-190-50-171.wireless.duke.edu>
2014-02-03 04:12:34 -0500

    Visc

---
+++ src/forces/CenterOfMass.java
@@ -0,0 +1,11 @@
+package forces;
+
+import jgame.platform.JGEngine;
+
+public class CenterOfMass {
+	JEngine game = new JEngine();
+	
+	double xCenter = displayWidth() / 2;
+	double yCenter = displayHeight() / 2;
+
+}
