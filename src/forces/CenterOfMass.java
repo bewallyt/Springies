ac7982669631d1b050aa750b79ca298cf5b1d7e3
Benson Tran <BewallyT@wl-10-190-50-171.wireless.duke.edu>
2014-02-03 06:23:14 -0500

    added center of mass

--- src/forces/CenterOfMass.java
+++ src/forces/CenterOfMass.java
@@ -1,12 +1,75 @@
 package forces;
 
-import jgame.platform.JGEngine;
+import java.util.ArrayList;
+
+import org.jbox2d.common.Vec2;
+
+import simulation.Mass;
 
 public class CenterOfMass {
 
-	
-	double xCenter = displayWidth() / 2;
+	private double xCenter;
+	private double yCenter;
+	private static ArrayList<Mass> myMasses = new ArrayList<Mass>();
 
-	double yCenter = displayHeight() / 2;
+	public CenterOfMass(ArrayList<Mass> masses) {
+		xCenter = 426;
+		yCenter = 240;
+		myMasses = masses;
+
+	}
+
+	public static Vec2 centerForce() {
+
+		ArrayList<Double> allX = new ArrayList<Double>();
+		ArrayList<Double> allY = new ArrayList<Double>();
+		double sumX = 0;
+		double sumY = 0;
+		double avgX = 0;
+		double avgY = 0;
+		double dist = 0;
+		int max = myMasses.size();
+
+		for (int i = 0; i < max; i++) {
+
+			allX.add(myMasses.get(i).getMassX());
+			allY.add(myMasses.get(i).getMassY());
+
+		}
+
+		for (int i = 0; i < max; i++) {
+
+			sumX = sumX + allX.get(i);
+			sumY = sumY + allY.get(i);
+
+		}
+
+		avgX = sumX / max;
+		avgY = sumY / max;
+
+		dist = Math.sqrt(Math.pow(avgX - 426, 2) + Math.pow(avgX - 240, 2));
+
+		double angle = Math.atan((avgX - 426) / (avgX - 240));
+		double xComp = Math.cos(angle) * dist * 100;
+		double yComp = Math.sin(angle) * dist * 100;
+		
+		Vec2 vec;
+
+		if (((avgX - 426) > 0) && ((avgY - 240) > 0)) {
+			vec = new Vec2((float) -xComp, (float) -yComp);
+		}
+		else if (((avgX - 426) < 0) && ((avgY - 240) > 0)) {
+			vec = new Vec2((float) xComp, (float) -yComp);
+		}
+		else if (((avgX - 426) > 0) && ((avgY - 240) < 0)) {
+			vec = new Vec2((float) -xComp, (float) yComp);
+		}
+		else{
+			vec = new Vec2((float) xComp, (float) yComp);
+		}
+
+		return vec;
+
+	}
 
 }
--- src/simulation/FixedMass.java
+++ src/simulation/FixedMass.java
@@ -5,7 +5,7 @@ public class FixedMass extends Mass {
 	/* Constructor */
 	public FixedMass(String id, double xCoord, double yCoord) {
 
-		super(id, 1.0, 1.0, xCoord, yCoord, 0, 0, 0);
+		super(id, 1.0, 1.0, xCoord, yCoord, 0, 0);
 		// Changed x and y to this.x and this.y
 		this.x = xCoord;
 		this.y = yCoord;
