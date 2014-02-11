df2643e57dd9308cde29737f209f2fc5f5e8b394
In-Young Jo <inyoungjo@wl-10-190-52-163.wireless.duke.edu>
2014-02-11 07:13:08 -0500

    colors for muscles and springs

--- src/simulation/Muscle.java
+++ src/simulation/Muscle.java
@@ -1,8 +1,9 @@
 package simulation;
 
+import jboxGlue.PhysicalObject;
 import jgame.JGColor;
 
-public class Muscle extends Spring {
+public class Muscle extends PhysicalObject {
 
 	/* Mass objects connected by the Spring */
 	private Mass myMass1, myMass2;
@@ -11,40 +12,65 @@ public class Muscle extends Spring {
 	private double myAmplitude;
 	private double radian = 0;
 	private int clear = 0;
-
+	private double dx;
+	private double dy;
+	private double dist;
+	private double magnitude;
+	private double xComp;
+	private double yComp;
+	JGColor color;
+	JGColor colorExpand;
+	JGColor colorContract;
+	
 	public Muscle(Mass m1, Mass m2, double restL, double K, double amp) {
-		super(m1, m2, restL, K);
+		//super(m1, m2, restL, K);
+		super("muscle", 0, JGColor.orange);
 		myMass1 = m1;
 		myMass2 = m2;
 		myRestLength = restL;
 		mySpringyness = K;
 		myAmplitude = amp;
+		color = JGColor.orange;
+		colorExpand = JGColor.yellow;
+		colorContract = JGColor.red;
 	}
 
 	@Override
 	protected void paintShape() {
 		if (clear != 1) {
-			radian += .1;
-			myEngine.setColor(JGColor.red);
-			myEngine.drawLine(myMass1.getMassX(), myMass1.getMassY(),
-					myMass2.getMassX(), myMass2.getMassY());
-			muscleForce();
+			if (dist == myRestLength) {
+				connectingLine(color);
+			}
+			else if(dist > myRestLength){
+				connectingLine(colorExpand);
+			}
+			else if(dist < myRestLength){
+				connectingLine(colorContract);
+			}
 		}
+	}
+	
+	protected void connectingLine(JGColor c)	{
+		myEngine.setColor(c);
+		myEngine.drawLine(myMass1.getMassX(), myMass1.getMassY(),
+				myMass2.getMassX(), myMass2.getMassY());
+		muscleForce();
 	}
 
 	public void muscleForce() {
+		radian += .1;
 		double tempRestLength = myRestLength + myAmplitude*Math.sin(radian)*5;
-		double dx = myMass2.getMassX() - myMass1.getMassX();
-		double dy = myMass2.getMassY() - myMass1.getMassY();
-		double dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+		dx = myMass2.getMassX() - myMass1.getMassX();
+		dy = myMass2.getMassY() - myMass1.getMassY();
+		dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 //		double sinForce = Math.sin(radian) * 250 * myAmplitude;
 //		double magnitude = (mySpringyness * (dist - myRestLength) * 70) +
 //				sinForce;
 //		double magnitude = (mySpringyness * (dist - myRestLength) * 4)
 //				+ sinForce;
-		double magnitude = mySpringyness * (dist - tempRestLength);
-		double xComp = dx / dist * magnitude;
-		double yComp = dy / dist * magnitude;
+		magnitude = mySpringyness * (dist - tempRestLength) * 500;
+		xComp = dx / dist * magnitude;
+		yComp = dy / dist * magnitude;
 
 		myMass1.setForce(xComp, yComp);
 		myMass2.setForce(-xComp, -yComp);
--- src/simulation/Spring.java
+++ src/simulation/Spring.java
@@ -1,89 +1,15 @@
 package simulation;
 
-import jboxGlue.PhysicalObject;
 import jgame.JGColor;
 
-public class Spring extends PhysicalObject {
-	/* Mass objects connected by the Spring */
-
-	private Mass myMass1, myMass2;
-	private double myRestLength;
-	private double mySpringyness;
-	private double dx;
-	private double dy;
-	private double dist;
-	private double magnitude;
-	private double xComp;
-	private double yComp;
-
-	private int clear = 0;
+public class Spring extends Muscle {
 
 	/* Spring constructor with springyness */
 	public Spring(Mass m1, Mass m2, Double restL, Double K) {
-		super("spring", 0, JGColor.blue);
-		myMass1 = m1;
-		myMass2 = m2;
-		myRestLength = restL;
-		mySpringyness = K;
-	}
-
-	@Override
-	protected void paintShape() {
-		if (clear != 1) {
-			if (dist == myRestLength) {
-				myEngine.setColor(JGColor.green);
-				myEngine.drawLine(myMass1.getMassX(), myMass1.getMassY(),
-						myMass2.getMassX(), myMass2.getMassY());
-				springForce();
-			}
-			else if(dist > myRestLength){
-				myEngine.setColor(JGColor.orange);
-				myEngine.drawLine(myMass1.getMassX(), myMass1.getMassY(),
-						myMass2.getMassX(), myMass2.getMassY());
-				springForce();
-			}
-			else if(dist < myRestLength){
-				myEngine.setColor(JGColor.yellow);
-				myEngine.drawLine(myMass1.getMassX(), myMass1.getMassY(),
-						myMass2.getMassX(), myMass2.getMassY());
-				springForce();
-			}
-		}
-
-	}
-
-	public void springForce() {
-
-<<<<<<< HEAD
-		dx = myMass2.getMassX() - myMass1.getMassX();
-		dy = myMass2.getMassY() - myMass1.getMassY();
-		dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-		magnitude = mySpringyness * (dist - myRestLength) * 1800;
-
-		xComp = dx / dist * magnitude;
-		yComp = dy / dist * magnitude;
-
-=======
-		double dx = myMass2.getMassX() - myMass1.getMassX();
-		double dy = myMass2.getMassY() - myMass1.getMassY();
-		double dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-		double magnitude = mySpringyness * (dist - myRestLength); //Daintywalker
-		//double magnitude = mySpringyness * (dist - myRestLength) * 1800; //Ball
-		double xComp = dx / dist * magnitude;
-		double yComp = dy / dist * magnitude;
-
->>>>>>> 77e876985b0248f153a80be14a2415126c75727f
-		myMass1.setForce(xComp, yComp);
-		myMass2.setForce(-xComp, -yComp);
-	}
-
-	public String toString() {
-		return myMass1 + " " + myMass2 + " rl:" + myRestLength + " K:"
-				+ mySpringyness;
-	}
-
-	public void terminate() {
-		clear = 1;
+		super(m1,m2,restL,K,0);
+		color = JGColor.blue;
+		colorExpand = JGColor.pink;
+		colorContract = JGColor.white;
 	}
 
 }
